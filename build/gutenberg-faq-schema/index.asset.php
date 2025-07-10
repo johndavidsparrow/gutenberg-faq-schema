6272ebd0206ced59347defaab2ddec80e38e10d4
John Sparrow <john.sparrow@gmail.com>
2025-07-09 17:26:08 -0700

    Remove default blue from styles

--- build/gutenberg-faq-schema/index.asset.php
+++ build/gutenberg-faq-schema/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9dff903da162eae823f9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ebc73a03c67b8fc26d1b');
