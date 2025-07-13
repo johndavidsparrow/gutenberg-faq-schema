5db63eddf7362cd0e7e39f945e4d7acbb0995ebf
John Sparrow <john.sparrow@gmail.com>
2025-07-12 18:39:40 -0700

    Production build

--- build/gutenberg-faq-schema/index.asset.php
+++ build/gutenberg-faq-schema/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '680afb451dd1f39dd6ba');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9b4d5560fd294c7b3307');
--- build/gutenberg-faq-schema/view.asset.php
+++ build/gutenberg-faq-schema/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '8da74d9493a987eb51d1');
+<?php return array('dependencies' => array(), 'version' => '2ededdc58079a7861143');
